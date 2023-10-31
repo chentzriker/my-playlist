b94c41008dd783384e4131e667ccde7cc7c547f7
ChenZakaim <chenzakaim@hilma.tech>
2023-10-31 17:54:01 +0200

    added playlist class and DB playlist (and songs) methods

--- js/DB.js
+++ js/DB.js
@@ -1,7 +1,73 @@
 class DataBase {
-    constructor(){
-        this.users =JSON.parse(localStorage.getItem("users")) || {}
-        this.playlists = JSON.parse(localStorage.getItem("playlists")) || {}
+    constructor() {
+        this.users = JSON.parse(localStorage.getItem("users")) || {idCount:0, users: []};
+        this.playlists = JSON.parse(localStorage.getItem("playlists")) || [];
     }
-    
+
+    getPlaylistById(id) {
+        for (let playlist of this.playlists) {
+            if (playlist.userId === id) {
+                return playlist;
+            }
+        }
+        return 404;
+    }
+    //this method adds a new song it gets to the user's playlist
+    addSongToPlaylist(userId, song) {
+        const USER_PLAYLIST = this.getPlaylistById(userId);
+        if (USER_PLAYLIST === 404) {
+            return 404;
+        }
+        USER_PLAYLIST.push(song);
+        localStorage.setItem("playlists", JSON.stringify(USER_PLAYLIST));
+        return 200;
+    }
+    editSong(userId, songId, key, value) {
+        const USER_PLAYLIST = this.getPlaylistById(userId);
+        let success = false;
+        if (USER_PLAYLIST === 404) {
+            return 404;
+        }
+        for (let song of USER_PLAYLIST) {
+            if (song.id === songId) {
+                song[key] = value;
+                success = song;
+            }
+        }
+        if (!success) {
+            return 404;
+        }
+        else {
+            localStorage.setItem("playlists", JSON.stringify(USER_PLAYLIST));
+            return success;
+        }
+    }
+    deleteSong(userId, songId) {
+        const USER_PLAYLIST = this.getPlaylistById(userId);
+        let index = -1;
+        if (USER_PLAYLIST === 404) {
+            return 404;
+        }
+        for (let i = 0; i < USER_PLAYLIST.length - 1; i++) {
+            if (USER_PLAYLIST[i].id === songId) {
+                index = i;
+            }
+        }
+        if (index = -1) {
+            return 404;
+        }
+        else {
+            USER_PLAYLIST.splice(index, 1);
+            localStorage.setItem("playlists", JSON.stringify(USER_PLAYLIST));
+            return 202;
+        }
+    }
+}
+DB = new DataBase();
+class Playlist {
+    constructor(userId, countSongsId = 0, songs = []) {
+        this.userId = userId;
+        this.countSongsId = countSongsId;
+        this.songs = songs;
+    }
 }
---
+++ js/server.js
@@ -0,0 +1,4 @@
+class server{
+
+}
+class 
