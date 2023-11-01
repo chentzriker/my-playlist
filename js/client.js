8958a9de2d3eef57247844327f9ea1e81a6349b0
ChenZakaim <chenzakaim@hilma.tech>
2023-11-01 11:37:30 +0200

    create request general method added

--- js/client.js
+++ js/client.js
@@ -1,10 +1,22 @@
-sendHTMLLoginRequest(username,password){
+function sendHTMLLoginRequest(username, password) {
     //create new FAJAX and send it
-    let request = new FXMLHttpRequest()
-    request.open("POST", "my-playlist.il/users")
-    request.onload  =function(){}
-    request.send(JSON.stringify({ name: username, password: password }))
+    let onload = () => {
+        if (this.status === 200) {
+            showContent(2)
+        }
+        else {
+            alert("one field or more is wrong")
+
+        }
+    };
+    createRequest("POST", "users", "", onload, JSON.stringify({ "name": username, "password": password }));
     //see if the server returnd that this user Exists or not
     //load the user page
-    showContent(2)
+}
+
+function createUserIdRequest(orderType, location, userId, onload, toChange = undefined) {
+    let request = new FXMLHttpRequest()
+    request.open(orderType, "/my-playlist.il.API/" + location + "/" + userId);
+    request.onload = onload;
+    request.send(JSON.stringify(toChange));
 }
