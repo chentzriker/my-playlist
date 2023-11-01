4a8612a1cf0f0af399aeaf247cd435a3abdd9b4a
ChenZakaim <chenzakaim@hilma.tech>
2023-11-01 11:08:45 +0200

    added database API comments and some server methods

--- js/DB.js
+++ js/DB.js
@@ -5,6 +5,46 @@ class DataBase {
 
     }
 
+    getUsers() {
+        return (this.users).users;
+    }
+    //put an id to the new user and add a user to the storage 
+    addUser(obj) {
+        let users = this.users;
+        let userId = users["idCount"];
+        obj.id = userId;
+        let playlists = this.playlists;
+        let newPlaylist = new Playlist(userId);
+        playlists.push(newPlaylist);
+        users.users.push(obj);
+        users["idCount"] = users["idCount"] + 1;
+        localStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("playlists", JSON.stringify(playlists));
+        return 200;
+    }
+
+    editUser(userId, key, value) {
+        let users = this.users;
+        for (const element of users.users) {
+            if (element.id === userId) {
+                element[key] === value;
+                localStorage.setItem("users", JSON.stringify(users));
+                return 200;
+            }
+        }
+        return 404
+    }
+
+    deleteUser(userId) {
+        let users = this.users
+        for (let i = 0; i < users.users.length; i++) {
+            if (users.users[i].id === userId) {
+                users.users.splice(i, 1);
+                return 200;
+            }
+        }
+        return 404
+    }
     getSongsListById(id) {
         for (let playlist of this.playlists) {
             if (playlist.userId === id) {
@@ -80,46 +120,6 @@ class DataBase {
         }
     }
 
-    getUsers() {
-        return (this.users).users
-    }
-    //put an id to the new user and add a user to the storage 
-    addUser(obj) {
-        let users = this.users
-        let userId = users["idCount"]
-        obj.id = userId
-        let playlists = this.playlists
-        let newPlaylist = new Playlist(userId)
-        playlists.push(newPlaylist)
-        users.users.push(obj)
-        users["idCount"] = users["idCount"] + 1
-        localStorage.setItem("users", JSON.stringify(users))
-        localStorage.setItem("playlists", JSON.stringify(playlists))
-        return 200
-    }
-
-    editUser(userId, key, value) {
-        let users = this.users
-        for (const element of users.users) {
-            if (element.id === userId) {
-                element[key] === value
-                localStorage.setItem("users", JSON.stringify(users))
-                return 200
-            }
-        }
-        return 404
-    }
-
-    deleteUser(userId) {
-        let users = this.users
-        for (let i = 0; i < users.users.length; i++) {
-            if (users.users[i].id === userId) {
-                users.users.splice(i, 1)
-                return 200
-            }
-        }
-        return 404
-    }
 }
 class Playlist {
     constructor(userId) {
--- js/server.js
+++ js/server.js
@@ -1,17 +1,59 @@
-class Server{
+//~ DATABASE API: ~
+// database name: DB
 
-}
+// user related methods:
+// * getUsers() - returns an array with all the users.
+// * addUser(obj) - gets a user object and adds it to the database. if fails, returns 404,
+// otherwise - returns 200
+// * editUser(userId, key, value) - changes the value of the key that belongs to the user with 
+// the given id. if fails, returns 404, otherwise - returns 200.
+// * deleteUser(userId) - deletes the the user with 
+// the given id. if fails, returns 404, otherwise - returns 200.
+
+// song / playlist related methods:
+// * getSongsListById(id) - returns the user's playlist by their id. if fails, returns 404.
+// * getPlaylistObj(id) - returns the user's playlist OBJECT (which means, an object with the user's id, 
+// id count for the songs and playlist array) by their id. if fails, returns 404.
+// * addSongToPlaylist(userId, song) - adds a given song to the user's playlist with their given id. 
+// if fails, returns 404. otherwise, 200.
+// * editSong(userId, songId, key, value)
+// * deleteSong(userId, songId) 
 class User{
     constructor(name,password){
         this.name = name
         this.password = password
-    }
-}
-
+    }    
+}    
 class song{
     constructor(title, artist, length){
         this.title = title;
         this.artist = artist;
         this.length = length;
+    }    
+}    
+class Server{
+    constructor(){};
+    getUsersArray() {
+        return DB.getUsers();
     }
-}
+    addUserToDB(name, password) {
+        ///FAJAX!! NEEDS TO BE CHANGED LATER!!
+        const ans = DB.addUser(new User(name, password));
+        if (ans === 404){
+            //change fajax textResponse
+        }
+        return ans;
+    }
+    editExsistingUserInDB(userId, key, value) {
+        ///FAJAX!! NEEDS TO BE CHANGED LATER!!
+        const ans = DB.editUser(userId, key, value);
+        if (ans === 404) {
+            //change fajax textResponse to error
+        }
+        return ans;//????? needs to return fajax
+    }
+}    
+
+
+
+
